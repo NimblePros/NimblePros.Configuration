8f7478285eab7036d48dbc164e18b7d47363e42e
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2023-03-28 11:36:10 +0100

    Add Ninject implementation

---
+++ src/NimblePros.Configuration.Ninject/KernelExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+
+using Ninject;
+using Ninject.Syntax;
+
+namespace NimblePros.Configuration.Ninject
+{
+  public static class KernelExtensions
+  {
+    public static IBindingWhenInNamedWithOrOnSyntax<T> AddSingletonConfig<T>(
+          this IKernel kernel,
+          IConfiguration configuration
+      )
+      where T : class, new()
+    {
+      var config = new T();
+      configuration.Bind(config);
+      return kernel.Bind<T>().ToConstant(config);
+    }
+
+    public static IBindingNamedWithOrOnSyntax<T> AddScopedConfig<T>(
+        this IKernel kernel,
+        IConfiguration configuration
+    )
+    where T : class, new()
+    {
+      return kernel.Bind<T>().ToMethod(context =>
+      {
+        var config = new T();
+        configuration.Bind(config);
+        return config;
+      }).InTransientScope();
+    }
+  }
+}
---
+++ tests/NimblePros.Configuration.Tests/Ninject/KernelExtensionsTests.cs
@@ -0,0 +1,70 @@
+using System.Collections.Concurrent;
+
+using NimblePros.Configuration.Ninject;
+using NimblePros.Configuration.Tests.Helpers;
+using NimblePros.Configuration.Unity;
+
+using Ninject;
+
+namespace NimblePros.Configuration.Tests.Unity;
+
+public class KernelExtensionsTests
+{
+  [Fact]
+  public void AddSingletonConfig()
+  {
+    // Arrange
+    IKernel kernel = new StandardKernel();
+    var inMemoryCollection = new ConcurrentDictionary<string, string?>
+    {
+      ["TestKey"] = "Initial"
+    };
+    var configuration = inMemoryCollection.SetupConfiguration();
+
+    // Act
+    kernel.AddSingletonConfig<TestConfig>(configuration);
+
+    var config1 = kernel.Get<TestConfig>();
+
+    inMemoryCollection["TestKey"] = "Updated";
+    configuration.Reload();
+
+    var config2 = kernel.Get<TestConfig>();
+
+    // Assert
+    config1.Should().NotBeNull();
+    config1.TestKey.Should().Be("Initial");
+    config2.Should().NotBeNull();
+    config2?.TestKey.Should().Be("Initial");
+    config1.Should().BeSameAs(config2);
+  }
+
+  [Fact]
+  public void AddScopedConfig()
+  {
+    // Arrange
+    IKernel kernel = new StandardKernel();
+    var inMemoryCollection = new ConcurrentDictionary<string, string?>
+    {
+      ["TestKey"] = "Initial"
+    };
+    var configuration = inMemoryCollection.SetupConfiguration();
+
+    // Act
+    kernel.AddScopedConfig<TestConfig>(configuration);
+
+    var config1 = kernel.Get<TestConfig>();
+
+    inMemoryCollection["TestKey"] = "Updated";
+    configuration.Reload();
+
+    var config2 = kernel.Get<TestConfig>();
+
+    // Assert
+    config1.Should().NotBeNull();
+    config1?.TestKey.Should().Be("Initial");
+    config2.Should().NotBeNull();
+    config2?.TestKey.Should().Be("Updated");
+    config1.Should().NotBeSameAs(config2);
+  }
+}
